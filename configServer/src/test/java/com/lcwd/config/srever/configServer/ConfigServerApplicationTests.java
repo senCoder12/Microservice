3ac868c7ee4beb05e3b3ef9ede0357217039e6d5
mayukhSen12 <mayukhsen448@gmail.com>
2023-12-02 00:36:16 +0530

    Config server setup

---
+++ configServer/src/test/java/com/lcwd/config/srever/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwd.config.srever.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
